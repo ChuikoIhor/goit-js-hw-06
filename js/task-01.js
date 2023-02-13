845793e7b930f4b73d5a102569c021820d618d56
ChuikoIhor <sheix91@icloud.com>
2023-02-13 13:59:01 +0200

    исправил первую задачку

--- js/task-01.js
+++ js/task-01.js
@@ -2,6 +2,6 @@ const totalCategoryEl = document.querySelectorAll('.item');
 console.log('Number of categories:', totalCategoryEl.length);
 
 totalCategoryEl.forEach(el => {
-  console.log('Category:', el.querySelector('h2').textContent);
-  console.log('Elements:', el.querySelectorAll('li').length);
+  console.log('Category:', el.firstElementChild.textContent);
+  console.log('Elements:', el.lastElementChild.children.length);
 });
